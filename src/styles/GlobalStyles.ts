5ed53a95e012bdd49faeb87b9c21a9d874111fb9
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2022-04-05 14:35:31 -0300

    feat: add google fonts

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -5,6 +5,8 @@ const GlobalStyles = createGlobalStyle`
   margin: 0;
   padding: 0;
   box-sizing: border-box;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
 }
 
 html, body, #root {
@@ -12,7 +14,7 @@ html, body, #root {
 }
 
 body {
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  font-family: 'Inter', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
 }
 `
 
