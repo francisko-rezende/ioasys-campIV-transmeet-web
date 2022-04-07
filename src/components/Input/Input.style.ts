040b457228f1874e70f72d308e3f6d21e6aa8e4b
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2022-04-07 12:07:40 -0300

    fix: make Inputs stop rendering on top of e/o

--- src/components/Input/Input.style.ts
+++ src/components/Input/Input.style.ts
@@ -3,14 +3,17 @@ import styled, { css } from 'styled-components'
 export const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
-  width: 470px;
+  width: inherit;
+  /* margin-bottom: 100px; */
+  /* width: 100%; */
+  /* background-color: blue; */
 `
 
 export const Label = styled.label`
   z-index: 2;
   position: relative;
   left: 27px;
-  top: -6px;
+  top: 10px;
   padding: 0 5px;
   width: fit-content;
 
@@ -23,16 +26,16 @@ export const Label = styled.label`
 `
 
 export const Input = styled.input`
-  position: absolute;
   border-width: 1px;
   border-color: ${({ theme }) => theme.colors.typographyColors.gray};
   border-style: solid;
   border-radius: 4px;
   padding: 14px 16px;
   height: 56px;
-  width: 470px;
-  margin-left: 15px;
-  max-width: 100%;
+  width: 100%;
+  /* width: 470px; */
+  /* margin-left: 15px; */
+  max-width: 470px;
   line-height: 1.4;
 
   ${({ theme }) => css`
@@ -44,7 +47,7 @@ export const Input = styled.input`
 export const Error = styled.p`
   color: ${({ theme }) => theme.colors.statusColor.error};
   position: relative;
-  top: 3rem;
+  top: 0.5rem;
   left: 29px;
   max-width: 100%;
 `
