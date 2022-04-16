606014cd6d669dd7f6f86ffc52046793d641ae5e
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2022-04-16 10:00:18 -0300

    feat: start api integration

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -3,11 +3,14 @@ import React from 'react'
 import { ReactComponent as LockIcon } from '../../assets/icons/lock.svg'
 import { ReactComponent as Logo } from '../../assets/icons/logo-central-color.svg'
 import { ReactComponent as MailIcon } from '../../assets/icons/mail.svg'
+import api from '../../Services/api'
 import Button from '../Button'
 import InputWithIcon from '../InputWithIcon'
 import * as S from './LoginForm.style'
 
 const LoginForm = () => {
+  // todo setup context for saving account info and token
+
   const [userEmail, setUserEmail] = React.useState('')
   const [password, setPassword] = React.useState('')
 
@@ -26,7 +29,7 @@ const LoginForm = () => {
           type="email"
           name="email"
           icon={<MailIcon />}
-          value={password}
+          value={'carla@gmail.com'} // todo replace with email state later
           onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
             setPassword(e.target.value)
           }
@@ -38,14 +41,43 @@ const LoginForm = () => {
           type="password"
           name="password"
           icon={<LockIcon />}
-          value={userEmail}
+          value={'Amarelo34'} // todo replace with password state later
           onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
             setUserEmail(e.target.value)
           }
         />
       </S.InputsWrapper>
       <S.Link>Esqueci minha senha</S.Link>
-      <Button length="full" color="secondary">
+      <Button
+        length="full"
+        color="secondary"
+        type="submit"
+        onClick={async (e) => {
+          e.preventDefault()
+
+          const {
+            data: { access_token, account },
+          } = await api.post('/login', {
+            email: 'carla@gmail.com',
+            password: 'Amarelo34',
+          })
+          // console.log(access_token, account)
+
+          const users = await api.get('/users', {
+            headers: {
+              authorization: `Bearer ${access_token}`,
+            },
+          })
+
+          // const sponsors = await api.get('/sponsors', {
+          //   headers: {
+          //     Authorization: token,
+          //   },
+          // })
+          console.log(users)
+          // console.log(sponsors)
+        }}
+      >
         Fazer login
       </Button>
       <S.RegisterText>
