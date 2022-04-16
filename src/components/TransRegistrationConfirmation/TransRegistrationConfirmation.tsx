5dd920376e48759f04a3202c844d0ed451122945
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2022-04-16 09:59:53 -0300

    feat: move trans person registration button from form's second page to general form component

--- src/components/TransRegistrationConfirmation/TransRegistrationConfirmation.tsx
+++ src/components/TransRegistrationConfirmation/TransRegistrationConfirmation.tsx
@@ -15,84 +15,6 @@ export type TransRegistrationConfirmationProps = {
   setAgreesWithTerms: React.Dispatch<React.SetStateAction<boolean>>
 }
 
-export type Payload = {
-  name: string
-  email: string
-  password: string
-  regNumber: string
-  birthDate?: string
-  address: string
-  gender?: string
-  description: string
-  telephone: string
-  city: string
-  stateId: number | undefined
-  typeId: 1 | 2
-}
-
-const getStateId = (stateName: string) =>
-  ({
-    Acre: 1,
-    Alagoas: 2,
-    Amapá: 3,
-    Amazonas: 4,
-    Bahia: 5,
-    Ceará: 6,
-    'Espírito Santo': 7,
-    Goiás: 8,
-    Maranhão: 9,
-    'Mato Grosso': 10,
-    'Mato Grosso do Sul': 11,
-    'Minas Gerais': 12,
-    Pará: 13,
-    Paraná: 14,
-    Paraíba: 15,
-    Pernambuco: 16,
-    Piauí: 17,
-    'Rio de Janeiro': 18,
-    'Rio Grande do Norte': 19,
-    'Rio Grande do Sul': 20,
-    Rondônia: 21,
-    Roraima: 22,
-    'Santa Catarina': 23,
-    'São Paulo': 24,
-    Sergipe: 25,
-    Tocantins: 26,
-    'Distrito Federal': 27,
-  }[stateName])
-
-const generatePayload = (inputs: TransInputs): Payload => {
-  const {
-    name,
-    email,
-    password,
-    birthDate,
-    address,
-    genderSelect: gender,
-    description,
-    city,
-    state,
-    areaCode,
-    phoneNumber,
-    cpf: regNumber,
-  } = inputs
-
-  return {
-    name,
-    email,
-    password,
-    regNumber,
-    birthDate,
-    address,
-    gender,
-    description,
-    telephone: `${areaCode}${phoneNumber}`,
-    city,
-    stateId: getStateId(state),
-    typeId: 1,
-  }
-}
-
 const TransRegistrationConfirmation = ({
   agreesWithTerms,
   setAgreesWithTerms,
@@ -152,9 +74,6 @@ const TransRegistrationConfirmation = ({
           setAgreesWithTerms={setAgreesWithTerms}
         />
       </S.Form>
-      <button onClick={() => console.log(generatePayload(inputs))}>
-        Imprime carai
-      </button>
     </>
   )
 }
--- src/components/TransRegistrationForm/TransRegistrationForm.tsx
+++ src/components/TransRegistrationForm/TransRegistrationForm.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import { ReactComponent as Chevron } from '../../assets/icons/Vector.svg'
+import api from '../../Services/api'
 import Button from '../Button'
 import { Pages } from '../RegistrationForm/RegistrationForm'
 import TransRegistrationConfirmation from '../TransRegistrationConfirmation'
@@ -29,7 +30,83 @@ export type TransInputs = {
   confirmPassword: string
 }
 
-//todo change page from number to discrete entity
+export type Payload = {
+  name: string
+  email: string
+  password: string
+  regNumber: string
+  birthDate?: string
+  address: string
+  gender?: string
+  description: string
+  telephone: string
+  city: string
+  stateId: number | undefined
+  typeId: 1 | 2
+}
+
+const getStateId = (stateName: string) =>
+  ({
+    Acre: 1,
+    Alagoas: 2,
+    Amapá: 3,
+    Amazonas: 4,
+    Bahia: 5,
+    Ceará: 6,
+    'Espírito Santo': 7,
+    Goiás: 8,
+    Maranhão: 9,
+    'Mato Grosso': 10,
+    'Mato Grosso do Sul': 11,
+    'Minas Gerais': 12,
+    Pará: 13,
+    Paraná: 14,
+    Paraíba: 15,
+    Pernambuco: 16,
+    Piauí: 17,
+    'Rio de Janeiro': 18,
+    'Rio Grande do Norte': 19,
+    'Rio Grande do Sul': 20,
+    Rondônia: 21,
+    Roraima: 22,
+    'Santa Catarina': 23,
+    'São Paulo': 24,
+    Sergipe: 25,
+    Tocantins: 26,
+    'Distrito Federal': 27,
+  }[stateName])
+
+const generatePayload = (inputs: TransInputs): Payload => {
+  const {
+    name,
+    email,
+    password,
+    birthDate,
+    address,
+    genderSelect: gender,
+    description,
+    city,
+    state,
+    areaCode,
+    phoneNumber,
+    cpf: regNumber,
+  } = inputs
+
+  return {
+    name,
+    email,
+    password,
+    regNumber,
+    birthDate,
+    address,
+    gender,
+    description,
+    telephone: `${areaCode}${phoneNumber}`,
+    city,
+    stateId: getStateId(state),
+    typeId: 1,
+  }
+}
 
 const TransRegistrationForm = ({
   currentPage,
@@ -82,7 +159,7 @@ const TransRegistrationForm = ({
           <Button
             color="secondary"
             length="full"
-            onClick={() => console.log(inputs)}
+            // onClick={() => api.post('/users', generatePayload(inputs))}
             disabled={!agreesWithTerms}
           >
             Criar conta
